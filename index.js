2294b48b2335d9f3d32a51ce1fb9bec55cbe24c1
Arifprogrammer <arifmgtcu@gmail.com>
2023-06-10 18:07:25 +0600

    integrated with mongodb

--- index.js
+++ index.js
@@ -7,10 +7,42 @@ const corsOptions = {
   credentials: true,
   optionSuccessStatus: 200,
 };
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 //* middlewares
 app.use(cors(corsOptions));
 app.use(express.json());
+
+//* ingrating with mongoDB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ketp048.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const usersCollection = client.db("athleteXDB").collection("users");
+    // Connect the client to the server	(optional starting in v4.7)
+    client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 //* get request for testing the server
 app.get("/", (req, res) => {
@@ -19,5 +51,5 @@ app.get("/", (req, res) => {
 
 //* testing the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
